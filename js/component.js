d8cfdbe0a0613d9b4826475449cf6e91d2b45693
fudhoil <71709328+fudhoil@users.noreply.github.com>
2021-11-24 13:51:39 +0700

    ok

--- js/component.js
+++ js/component.js
@@ -2,3 +2,6 @@ $.get("header.html", function(data){
     $("#header-placeholder").replaceWith(data);
 });
 
+$.get("footer.html", function(data){
+    $("#footer-placeholder").replaceWith(data);
+});
